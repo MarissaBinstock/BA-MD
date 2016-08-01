267eabb8c6fd204779265b8c5a7e86a0c9297043
Marissa Binstock <marissabinstock@hotmail.com>
2016-08-01 08:49:52 -0400

    complete redesign

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+// $(document).ready(function(){
+// 	console.log("Hello World");
+// 	$("section").hide();
+// })
+
+$(document).ready(function(){
+	console.log("working");
+  // $(".message").click(function(){
+  // 	console.log("working2")
+  //   	alert('Thank you!');
+  //   });
+  // $(".about-us img").hide();
+  // $("h4").click(function() {
+  // 	$(".about-us img").fadeIn("slow")
+  // })
+
+  $("button").click(function() {
+  	$("map").toggle(1000);
+  });
+
+  	
+});
+
+
+
